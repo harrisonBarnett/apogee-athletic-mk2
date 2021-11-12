5e1d4598eb2b6f3c6b9bfda88c328662acde7f4b
harrisonBarnett <74162979+harrisonBarnett@users.noreply.github.com>
2021-11-11 23:06:50 -0500

    Playing with shopify

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,19 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import Footer from './swiper-main-components/Footer';
 const Shop = () => {
     return (
         <div id='section-shop'>
             <div id='shop-left'>
-            <h3
-            id='shop-title'>shop.</h3>
+                <h3
+                id='shop-title'>shop.</h3>
             </div>
+            
             <div id='shop-right'></div>
-            <div
-            id='shop-container'>
-            </div>
             <Footer />
         </div>
     );
 };
 
 export default Shop;
+
