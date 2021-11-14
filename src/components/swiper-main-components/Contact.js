a76b201ccc0320d9b5dc144a8c3c51ddfbcdd77f
harrisonBarnett <74162979+harrisonBarnett@users.noreply.github.com>
2021-11-14 11:51:23 -0500

    Big styles; assets

--- src/components/swiper-main-components/Contact.js
+++ src/components/swiper-main-components/Contact.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import Footer from './Footer'
 
-import Spotify from '../../static/images/social-icons/spotify.svg'
-import Apple from '../../static/images/social-icons/apple.svg'
-import PodcastAddict from '../../static/images/social-icons/podcastaddict.svg'
-import Youtube from '../../static/images/social-icons/youtube-red.svg'
+import Spotify from '../../static/images/social-icons/spotify.png'
+import Apple from '../../static/images/social-icons/apple.png'
+import PodcastAddict from '../../static/images/social-icons/podcastaddict.png'
+import Youtube from '../../static/images/social-icons/youtube-red.png'
+
+import Instagram from '../../static/images/social-icons/instagram.svg'
 
 import { db } from '../../firebase_config'
 import { collection, addDoc } from 'firebase/firestore'
@@ -19,23 +21,40 @@ const Contact = () => {
         emailField.value = ''
     }
     return (
-        <div>
-            <h1>contact slide</h1>
-            <p id='contact-copy'>listen along with the Apogee Fitness Podcast</p>
-            <p id='contact-copy-subtitle'>powered by Anchor.fm</p>
-            <div id='listen-grid'>
-                <div id='spotify-btn' className='listen-img' style={{backgroundImage: `url(${Spotify})` }}></div>
-                <div id='apple-btn' className='listen-img' style={{backgroundImage: `url(${Apple})` }}></div>
-                <div id='podcast-addict-btn' className='listen-img' style={{backgroundImage: `url(${PodcastAddict})`}}></div>
-                <div id='youtube-btn' className='listen-img' style={{backgroundImage: `url(${Youtube})`}}></div>
+        <>
+            <div id='contact-left'></div>
+            <div id='contact-right'>
+                <h2>follow along for updates</h2>
+                <div id='social-group'>
+                    <div className='social-img'></div>
+                    <div className='social-img'></div>
+                    <div className='social-img'></div>
+                </div>
+                <div id='signup-group'>
+                    <input 
+                    id='input-field'
+                    placeholder={'email...'}
+                    onChange={(event) => setNewEmail(event.target.value)}/>
+                    <button onClick={submitEmail}>stay updated</button>
+                </div>
+                <div id='contact-copy-group'>
+                    <p id='contact-copy'>listen along with the Apogee Fitness Podcast</p>
+                    <p id='contact-copy-subtitle'>powered by Anchor.fm</p>
+                </div>
+                <div id='listen-grid-perspective'>
+                    <div id='listen-grid'>
+                        <img src={Spotify} className='listen-img'></img>
+                        <img src={Apple} className='listen-img'></img>
+                        <img src={PodcastAddict} className='listen-img'></img>
+                        <img src={Youtube} className='listen-img'></img>
+                    </div>
+                </div>
+                
             </div>
-            <input 
-            id='input-field'
-            placeholder={'email...'}
-            onChange={(event) => setNewEmail(event.target.value)}/>
-            <button onClick={submitEmail}>sign up</button>
+            
+            
             <Footer />
-        </div>
+        </>
     );
 };
 
